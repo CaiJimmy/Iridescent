c8cb844b9e102838f9989f70831257e6d17b76cd
Jimmy Cai <jimmehcai@gmail.com>
2017-12-02 09:42:32 +0100

    fix(topic settings): improve loading experience

--- src/components/Settings/Topics.vue
+++ src/components/Settings/Topics.vue
@@ -1,5 +1,67 @@
 <template>
-	<div class="container">
+	<div>
+		<md-progress-bar class="md-primary"
+		    md-mode="indeterminate"
+		    v-if="loading.levels"></md-progress-bar>
+
+		<div class="container" v-else>
+			<md-empty-state v-if="!levels.length"
+			    md-icon="add"
+			    md-label="Crear nivel"
+			    md-description="Para comenzar, crear un nivel">
+				<md-button class="md-primary md-raised"
+				    v-on:click="showDialog.addLevel = true">Crear</md-button>
+			</md-empty-state>
+
+			<div v-else>
+				<md-list class="md-elevation-1 levelList"
+				    v-for="level in levels"
+				    :key="level.id">
+					<md-subheader>
+						<h1 class="md-title"
+						    style="flex: 1;">{{ level.name }}</h1>
+						<md-button v-on:click="addTopic(level.id)"
+						    class="md-icon-button md-list-action">
+							<md-icon class="md-primary">add</md-icon>
+						</md-button>
+					</md-subheader>
+
+					<md-progress-spinner md-mode="indeterminate"
+					    v-if="loading.topics"></md-progress-spinner>
+
+					<div v-else>
+						<md-empty-state v-if="!topicFilter(level.id).length"
+						    md-icon="add"
+						    md-label="Nivel vacío"
+						    md-description="Añade temas a este nivel pulsando el botón de la esquina derecha">
+						</md-empty-state>
+
+						<md-list-item v-for="topic in topicFilter(level.id)"
+						    :key="topic.id">
+							<md-avatar v-if="topic.image">
+								<img :src="topic.image"
+								    :alt="topic.name">
+							</md-avatar>
+
+							<div class="md-list-item-text">
+								<span>{{ topic.name }}</span>
+							</div>
+
+							<md-button class="md-icon-button md-list-action"
+							    v-on:click="$router.push('/t/' + topic.id)">
+								<md-icon class="md-primary">remove_red_eye</md-icon>
+							</md-button>
+						</md-list-item>
+					</div>
+				</md-list>
+				<md-button class="md-fab"
+				    v-on:click="showDialog.addLevel = true"
+				    id="addLevel">
+					<md-tooltip md-direction="left">Añadir Nivel</md-tooltip>
+					<md-icon>add</md-icon>
+				</md-button>
+			</div>
+		</div>
 		<md-dialog :md-active.sync="showDialog.addTopic">
 			<AddTopic v-bind:selectedLevel="selectedLevel"
 			    v-bind:callback="closeAddTopicDialog" />
@@ -9,58 +71,6 @@
 			<AddLevel v-bind:callback="closeAddLevelDialog" />
 		</md-dialog>
 
-		<md-empty-state v-if="!levels.length"
-		    md-icon="add"
-		    md-label="Crear nivel"
-		    md-description="Para comenzar, crear un nivel">
-			<md-button class="md-primary md-raised"
-			    v-on:click="showDialog.addLevel = true">Crear</md-button>
-		</md-empty-state>
-
-		<div v-else>
-			<md-list class="md-elevation-1 levelList"
-			    v-for="level in levels"
-			    :key="level.id">
-				<md-subheader>
-					<h1 class="md-title"
-					    style="flex: 1;">{{ level.name }}</h1>
-					<md-button v-on:click="addTopic(level.id)"
-					    class="md-icon-button md-list-action">
-						<md-icon class="md-primary">add</md-icon>
-					</md-button>
-				</md-subheader>
-
-				<md-empty-state v-if="!topicFilter(level.id).length"
-				    md-icon="add"
-				    md-label="Nivel vacío"
-				    md-description="Añade temas a este nivel pulsando el botón de la esquina derecha">
-				</md-empty-state>
-
-				<md-list-item v-for="topic in topicFilter(level.id)"
-				    :key="topic.id">
-					<md-avatar v-if="topic.image">
-						<img :src="topic.image"
-						    :alt="topic.name">
-					</md-avatar>
-
-					<div class="md-list-item-text">
-						<span>{{ topic.name }}</span>
-					</div>
-
-					<md-button class="md-icon-button md-list-action"
-					    v-on:click="$router.push('/t/' + topic.id)">
-						<md-icon class="md-primary">remove_red_eye</md-icon>
-					</md-button>
-				</md-list-item>
-			</md-list>
-
-			<md-button class="md-fab"
-			    v-on:click="showDialog.addLevel = true"
-			    id="addLevel">
-				<md-tooltip md-direction="left">Añadir Nivel</md-tooltip>
-				<md-icon>add</md-icon>
-			</md-button>
-		</div>
 	</div>
 </template>
 <script>
@@ -86,7 +96,8 @@ export default {
 			topics: [],
 			levels: [],
 			loading: {
-				topics: true
+				topics: true,
+				levels: true
 			},
 
 			showDialog: {
@@ -98,13 +109,12 @@ export default {
 		};
 	},
 	created: function () {
-		this.$bind("topics", firebase.firestore().collection("topics"));
-		this.$bind("levels", firebase.firestore().collection("levels"));
-	},
-	watch: {
-		"topics": function () {
+		this.$bind("topics", firebase.firestore().collection("topics")).then(() => {
 			this.loading.topics = false;
-		}
+		});
+		this.$bind("levels", firebase.firestore().collection("levels")).then(() => {
+			this.loading.levels = false;
+		});;
 	},
 	methods: {
 		closeAddLevelDialog: function (dialogRef) {
