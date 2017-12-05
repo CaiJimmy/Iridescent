d32585c1e1ddc89dbc284002a3bedd714ed94971
Jimmy Cai <jimmehcai@gmail.com>
2017-12-05 18:16:00 +0100

    Fix(Profile): topic and user are undefined

--- src/components/Profile/QuestionCard.vue
+++ src/components/Profile/QuestionCard.vue
@@ -69,6 +69,14 @@ export default {
     components: {
         QuestionForm
     },
+    computed:{
+        users(){
+            return this.$store.state.users
+        },
+        topics(){
+            return this.$store.state.topics
+        }
+    },
     methods: {
         exitEditing () {
             this.editing = false
