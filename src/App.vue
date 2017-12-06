0cbd2e430a06f84827fc4adbb0a342148105f46a
Jimmy Cai <jimmehcai@gmail.com>
2017-12-06 14:01:58 +0100

    goBack() now only returns home

--- src/App.vue
+++ src/App.vue
@@ -108,9 +108,7 @@ export default {
 	},
 	methods: {
 		goBack () {
-			window.history.length > 1
-				? this.$router.go(-1)
-				: this.$router.push('/')
+			this.$router.push('/')
 		},
 		redirect: function () {
 			if (this.$route.query.go) {
