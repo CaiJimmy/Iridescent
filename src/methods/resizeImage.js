63ebae888021b557ac2647e81055d6729f5a923b
Jimmy Cai <jimmehcai@gmail.com>
2018-05-11 16:34:08 +0200

    Feat: new image compress system

--- src/methods/resizeImage.js
+++ src/methods/resizeImage.js
@@ -1,36 +1,16 @@
-export default function (file, perferedWidth) {
+import ImageCompressor from 'image-compressor.js';
+
+export default function (file, width) {
 	return new Promise((resolve, reject) => {
-		let reader = new FileReader();
-
-		reader.onload = function (e) {
-			var img = new Image();
-			img.onload = async function () {
-				let blob = await resizeInCanvas(img);
-				resolve(blob)
-			};
-			img.src = e.target.result;
-		};
-
-		reader.readAsDataURL(file);
-
-		function resizeInCanvas(img) {
-			var canvas = document.createElement('canvas');
-			var perferedWidth = perferedWidth | 1500;
-			var ratio = perferedWidth / img.width;
-			canvas.width = img.width * ratio;
-			canvas.height = img.height * ratio;
-			var ctx = canvas.getContext("2d");
-			ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-
-			function getCanvasBlob(canvas) { /// Promisify it
-				return new Promise((resolve, reject) => {
-					canvas.toBlob(function (blob) {
-						resolve(blob)
-					})
-				})
-			};
-
-			return getCanvasBlob(canvas)
-		}
+		new ImageCompressor(file, {
+			quality: .6,
+			maxWidth: width,
+			success(result) {
+				resolve(result);
+			},
+			error(e) {
+				console.log(e.message);
+			},
+		});
 	});
 };
