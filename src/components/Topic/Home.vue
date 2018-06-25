e4330a5af28d501163f7aa70cdd94c1f4b3ba6e2
Jimmy Cai <jimmehcai@gmail.com>
2018-06-25 15:59:42 +0200

    refactor(Topic/Home): remove unused `question_bar`

--- src/components/Topic/Home.vue
+++ src/components/Topic/Home.vue
@@ -133,9 +133,6 @@ export default {
         newQuestions(){
             return this.$parent.newQuestions;
         },
-        question_bar: function () {
-            return this.$parent.question_bar;
-        },
         questions: function () {
             return (this.$parent.questions)
         },
