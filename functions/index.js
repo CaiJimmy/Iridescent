f9bae8a92ae64a7269b28dfae3b0c65333987c9f
Jimmy Cai <jimmehcai@gmail.com>
2018-06-25 14:24:38 +0200

    feat: firebase functions to count questions
    - Add a new field under each topic's document: count(object)
    - When questions are modified, Firebase Function should update counter
    - For old topics, we can update counter using HTTP function `reCount`

---
+++ functions/index.js
@@ -0,0 +1,172 @@
+'use strict';
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+const db = admin.firestore();
+
+exports.questionChange = functions.firestore.document('questions/{questionID}').onWrite(
+	(change) => {
+
+		const oldQuestion = change.before.data(),
+			newQuestion = change.after.data();
+
+		let oldTopic,
+			oldTopicRef,
+			newTopic,
+			newTopicRef;
+
+		/* Load variables */
+		if (change.before.exists) {
+			oldTopic = oldQuestion.topic;
+			oldTopicRef = db.collection('topics').doc(oldTopic);
+		};
+		if (change.after.exists) {
+			newTopic = newQuestion.topic;
+			newTopicRef = db.collection('topics').doc(newTopic);
+		}
+
+
+		if (!change.before.exists) {
+			// New document Created : plus one to count.total
+
+			newTopicRef.get().then(snap => {
+				newTopicRef.set({
+					count: {
+						total: snap.data().count.total + 1
+					}
+				}, {
+					merge: true
+				});
+			});
+
+		} else if (change.before.exists && change.after.exists) {
+			// Updating existing document
+			// Two Cases:
+			// 1. Question was moved from one topic to another
+			// 2. Changed visibility
+
+			if (oldTopic !== newTopic) { /* Case 1 */
+
+				/// Subtract one from old topic count.total
+				oldTopicRef.get().then(snap => {
+					oldTopicRef.set({
+						count: {
+							total: snap.data().count.total - 1
+						}
+					}, {
+						merge: true
+					});
+				});
+
+				if (oldQuestion.hidden == true) {
+					/// Subtract one from old topic count.hidden if that question was hidden
+					oldTopicRef.get().then(snap => {
+						oldTopicRef.set({
+							count: {
+								hidden: snap.data().count.hidden - 1
+							}
+						}, {
+							merge: true
+						});
+					});
+				};
+
+				/// Plus one to new topic count.total
+				newTopicRef.get().then(snap => {
+					newTopicRef.set({
+						count: {
+							total: snap.data().count.total + 1
+						}
+					}, {
+						merge: true
+					});
+				});
+
+				if (newQuestion.hidden == true) {
+					/// Plus one to new topic count.hidden if that question is hidden
+					newTopicRef.get().then(snap => {
+						newTopicRef.set({
+							count: {
+								hidden: snap.data().count.hidden + 1
+							}
+						}, {
+							merge: true
+						});
+					});
+				};
+			} else if (oldQuestion.hidden !== newQuestion.hidden) { /* Case 2 */
+				/*
+					If isHidden is true, that means the question **was** visible. 
+				*/
+
+				const isHidden = newQuestion.hidden;
+
+				newTopicRef.get().then(snap => {
+					newTopicRef.set({
+						count: {
+							hidden: snap.data().count.hidden + (isHidden ? +1 : -1)
+						}
+					}, {
+						merge: true
+					});
+				});
+			}
+
+			return;
+
+		} else if (!event.data.exists) {
+			// Deleting question : subtract one from count.total and count.hidden if the question was hidden
+
+			const wasHidden = oldQuestion.hidden;
+
+			oldTopicRef.get().then(snap => {
+				oldTopicRef.set({
+					count: {
+						hidden: snap.data().count.hidden + (wasHidden ? -1 : 0),
+						total: snap.data().count.total - 1
+					}
+				}, {
+					merge: true
+				});
+			});
+		}
+	});
+
+exports.reCount = functions.https.onRequest((req, res) => {
+	const topicID = req.query.topic,
+		topicRef = db.collection('topics').doc(topicID);
+
+	if (!topicID) {
+		res.send("Topic ID is required");
+		return;
+	};
+
+	const allQuestions = db.collection('questions').where('topic', '==', topicID);
+
+	allQuestions.get().then(snap => {
+		let totalCount = snap.size,
+			hiddenCount = 0;
+
+		snap.forEach(item => {
+			if (item.data().hidden == true) {
+				hiddenCount++;
+			};
+		});
+
+		topicRef.set({
+			'count': {
+				total: totalCount,
+				hidden: hiddenCount
+			}
+		}, {
+			merge: true
+		}).then(() => {
+			res.json({
+				total: totalCount,
+				hidden: hiddenCount
+			});
+		});
+	})
+
+});
