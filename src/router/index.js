de02fa2b1fc7f8a78ed4b67a1b14fc15bf185d69
Jimmy Cai <jimmehcai@gmail.com>
2018-09-29 00:10:04 +0200

    feat(router): lazyload components

--- src/router/index.js
+++ src/router/index.js
@@ -8,141 +8,133 @@ import store from '@/store';
 Vue.use(Meta)
 Vue.use(Router)
 
+function loadComponent(view) {
+    return () =>
+        import( /* webpackChunkName: "view-[request]" */ `@/components/${view}.vue`)
+}
+
 var router = new Router({
-	routes: [{
-			path: '*',
-			component: require('@/components/NotFound.vue').default,
-			meta: {
-				hideNav: true
-			}
-		},
-		{
-			path: '/403',
-			component: require('@/components/NotAllowed.vue').default,
-			meta: {
-				hideNav: true
-			}
-		},
-		{
-			path: '/',
-			component: () =>
-				import ('@/components/Home.vue'),
-			auth: true
-		},
-		{
-			path: '/admin/',
-			component: () =>
-				import ('@/components/Admin/App.vue'),
-			auth: true,
-			meta: {
-				isAdmin: true
-			},
-			children: [{
-					path: '',
-					component: () =>
-						import ('@/components/Admin/Home.vue')
-				},
-				{
-					path: 'taxonomy',
-					component: () =>
-						import ('@/components/Admin/Taxonomy/App.vue'),
-				},
-				{
-					path: 'export',
-					component: () =>
-						import ('@/components/Admin/Export/App.vue'),
-				},
-				{
-					path: 'bulk',
-					component: () =>
-						import ('@/components/Admin/Bulk/App.vue'),
-				}
-			]
-		},
-		{
-			path: '/t/:topicID/',
-			component: () =>
-				import ('@/components/Topic/App.vue'),
-			auth: true,
-			props: true,
-			children: [{
-					path: '',
-					component: () =>
-						import ('@/components/Topic/Home.vue')
-				},
-				{
-					path: 'exam',
-					component: () =>
-						import ('@/components/Exam/App.vue'),
-					children: [{
-							path: '',
-							component: () =>
-								import ('@/components/Exam/Home.vue')
-						}, {
-							path: 'progress',
-							component: () =>
-								import ('@/components/Exam/InProgress.vue'),
-							meta: {
-								'returnDisabled': true
-							},
-						},
-						{
-							path: 'results',
-							component: () =>
-								import ('@/components/Exam/Results.vue'),
-							meta: {
-								'returnDisabled': true
-							},
-						}
-					]
-				}
-			]
-		},
-		{
-			path: '/login',
-			component: require('@/components/Login.vue').default
-		},
-		{
-			path: '/profile/:userID',
-			component: () =>
-				import ('@/components/Profile/App.vue'),
-			props: true,
-			meta: {
-				"hideToolbarTitle": true,
-				"toolbarElevation": 0
-			}
-		}
-	]
+    routes: [{
+            path: '*',
+            component: require('@/components/NotFound.vue').default,
+            meta: {
+                hideNav: true
+            }
+        },
+        {
+            path: '/403',
+            component: require('@/components/NotAllowed.vue').default,
+            meta: {
+                hideNav: true
+            }
+        },
+        {
+            path: '/',
+            component: loadComponent('Home'),
+            auth: true
+        },
+        {
+            path: '/admin/',
+            component: loadComponent('Admin/App'),
+            auth: true,
+            meta: {
+                isAdmin: true
+            },
+            children: [{
+                    path: '',
+                    component: loadComponent('Admin/Home')
+                },
+                {
+                    path: 'taxonomy',
+                    component: loadComponent('Admin/Taxonomy/App'),
+                },
+                {
+                    path: 'export',
+                    component: loadComponent('Admin/Export/App'),
+                },
+                {
+                    path: 'bulk',
+                    component: loadComponent('Admin/Bulk/App'),
+                }
+            ]
+        },
+        {
+            path: '/t/:topicID/',
+            component: loadComponent('Topic/App'),
+            auth: true,
+            props: true,
+            children: [{
+                    path: '',
+                    component: loadComponent('Topic/Home')
+                },
+                {
+                    path: 'exam',
+                    component: loadComponent('Exam/App'),
+                    children: [{
+                            path: '',
+                            component: loadComponent('Exam/Home')
+                        }, {
+                            path: 'progress',
+                            component: loadComponent('Exam/InProgress'),
+                            meta: {
+                                'returnDisabled': true
+                            },
+                        },
+                        {
+                            path: 'results',
+                            component: loadComponent('Exam/Results'),
+                            meta: {
+                                'returnDisabled': true
+                            },
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            path: '/login',
+            component: require('@/components/Login.vue').default
+        },
+        {
+            path: '/profile/:userID',
+            component: loadComponent('Profile/App'),
+            props: true,
+            meta: {
+                "hideToolbarTitle": true,
+                "toolbarElevation": 0
+            }
+        }
+    ]
 })
 
 router.beforeEach(function (to, from, next) {
-	if (to.auth && !firebase.auth().currentUser) {
-		next({
-			path: '/login',
-			query: {
-				go: to.path,
-			}
-		})
-	} else {
-		if (to.matched.some(m => m.meta.isAdmin) && !store.state.user.isAdmin && !store.state.loading.user) {
-			next({
-				path: '/403'
-			})
-		} else {
-			next();
-		}
-	}
+    if (to.auth && !firebase.auth().currentUser) {
+        next({
+            path: '/login',
+            query: {
+                go: to.path,
+            }
+        })
+    } else {
+        if (to.matched.some(m => m.meta.isAdmin) && !store.state.user.isAdmin && !store.state.loading.user) {
+            next({
+                path: '/403'
+            })
+        } else {
+            next();
+        }
+    }
 });
 
 router.afterEach(function (to, from, next) {
-	if (to.path !== '/login' && !firebase.auth().currentUser) {
-		router.replace({
-			path: '/login',
-			query: {
-				go: to.fullPath,
-			},
-		});
-	}
+    if (to.path !== '/login' && !firebase.auth().currentUser) {
+        router.replace({
+            path: '/login',
+            query: {
+                go: to.fullPath,
+            },
+        });
+    }
 })
 
 export default router
