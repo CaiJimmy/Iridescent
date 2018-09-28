f9896175e0058a7437331104e317f67ee338736b
Jimmy Cai <jimmehcai@gmail.com>
2018-09-28 20:44:06 +0200

    refactor(Topic/Form): remove db import temporary
    It's necessary to invoke firebase.firestore.FieldValue.serverTimestamp()

--- src/components/Topic/Form.vue
+++ src/components/Topic/Form.vue
@@ -84,8 +84,6 @@ import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 import "firebase/auth";
 
-import db from '@/firebase/database';
-
 export default {
     name: 'Send',
     props: ['topicRef', 'snackbar', 'callback', 'type', 'questionData', 'questionID'],
